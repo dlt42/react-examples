3837ee8443c9a1825ec93f9497adc024029eb9da
Daniel Taylor <33705640+dlt42@users.noreply.github.com>
2022-11-11 10:43:36 +0000

    JSON tree - components are now wrapped with React.memo to prevent unnecessary re-rendering, now limited to page width, arrays now have scroll bars, added a colour coded key

--- src/examples/json-viewer/JSONTree.tsx
+++ src/examples/json-viewer/JSONTree.tsx
@@ -1,71 +1,100 @@
+
 import React from "react";
 import { useState } from "react";
 
 
 interface JSONTreeProps {
-  depth: number,
   elemKey: string | null,
-  elemValue: Record<string, any> | number | string | boolean
+  elemValue: Record<string, any> | number | string | boolean,
+  isTop?: boolean
 }
 
 interface JSONArrayItemProps {
-  depth: number,
   elemValue: Record<string, any> | number | string | boolean
 }
 
-const getStyle = (val: number) => {
-  const hex = ((val * 20) % 256).toString(16);
-  return { borderColor: `#${hex}${hex}${hex}` }
-}
-
-const JSONBlock: React.FC<JSONTreeProps> = ({ elemKey, elemValue, depth }): JSX.Element => {
-  const [toggle, setToggle] = useState(depth === 0 || !elemKey);
-  const isArray = Array.isArray(elemValue);
+const JSONBlock: React.FC<JSONTreeProps> = React.memo((props): JSX.Element => {
+  const { elemKey, elemValue, isTop } = props;
+  const [ toggle, setToggle ] = useState(isTop || !elemKey);
+  const [ isArray ] = useState(Array.isArray(elemValue));
   return (
     <>
       { elemKey && 
         <div className="Json-Attribute-Name">
-          {elemKey}
-          <button onClick={(e) => setToggle(!toggle) }>{toggle ? '-' : '+' }</button>
+          { elemKey }
+          <button onClick={(e) => setToggle(!toggle) }>
+            {toggle ? '-' : '+' }
+          </button>
         </div> }
-      { !isArray && toggle &&
-        <div className="Json-Object" style={getStyle(depth +1)}>
-          { Object.entries(elemValue).map(([currentKey, currentValue], index) => ( 
-              <JSONTree key={index + currentKey} elemKey={currentKey} elemValue={currentValue} depth={depth + 2}/>
-            )) }
-        </div> }
-      { isArray && toggle &&
-        <div className="Json-Array" style={getStyle(depth +1)}>
-          { elemValue.map((current: any, index: number) => {
-              return ( <JSONArrayItem key={index} elemValue={current} depth={depth + 2}/>);
-          }) }
+      { 
+        !isArray && toggle &&
+        <div className="Json-Object">
+          { 
+            Object.entries(elemValue).map(([currentKey, currentValue], index) => (
+              <JSONTree key={index + currentKey} elemKey={currentKey} elemValue={currentValue} isTop={false}/>
+            ))
+          }
+        </div> 
+      }
+      { 
+        isArray && toggle &&
+        <div className="Json-Array">
+          { 
+            (elemValue as Record<string, any>).map((current: any, index: number) => (
+              <JSONArrayItem key={index} elemValue={current} /> 
+            ))
+          }
         </div>
       }
     </>
   )
-}
+})
 
-const JSONArrayItem: React.FC<JSONArrayItemProps> = (props): JSX.Element => {
+const JSONArrayItem: React.FC<JSONArrayItemProps> = React.memo((props): JSX.Element => {
   const { elemValue } = props;
-  const isObjectType = elemValue !== null && typeof elemValue === "object";
+  const [ hasValue ] = useState(elemValue !== undefined && elemValue !== null);
+  const [ isObjectType ] = useState(hasValue && typeof elemValue === "object");
   return (
     <>
-      { !isObjectType && <div className="Json-Attribute-Value">{ (elemValue || "").toString() }</div> }
-      { isObjectType && <JSONBlock {...props} elemKey={null} /> }
+      { 
+        !isObjectType && 
+        <div className="Json-Attribute-Value">
+          { (hasValue && elemValue.toString()) || "null" }
+        </div> 
+      }
+      { 
+        isObjectType && 
+        <JSONBlock {...props} elemKey={null} isTop={false} /> 
+      }
     </>
   )
-}
+})
 
-const JSONTree: React.FC<JSONTreeProps> = (props): JSX.Element => {
+const JSONTree: React.FC<JSONTreeProps> = React.memo((props): JSX.Element => {
   const { elemKey, elemValue } = props;
-  const isObjectType = elemValue !== null && typeof elemValue === "object";
+  const hasValue = elemValue !== undefined && elemValue !== null;
+  const isObjectType = hasValue && typeof elemValue === "object";
   return (
     <div className="Json-Attribute">
-      { !isObjectType && <div className="Json-Attribute-Name">{elemKey}</div> }
-      { !isObjectType && <div className="Json-Attribute-Value">{ (elemValue + "").toString() }</div> }
-      { isObjectType && <JSONBlock {...props} /> }
+      { 
+        !isObjectType && 
+          <div className="Json-Attribute-Name">
+            {elemKey}
+          </div>
+      }
+      { 
+          !isObjectType && 
+          <div className="Json-Attribute-Value">
+              { (hasValue && elemValue.toString()) || "null" }
+          </div> 
+      }
+      { 
+        isObjectType && (
+          <JSONBlock {...props} /> 
+        )
+      }
     </div>
   )
-}
+})
 
 export default JSONTree;
--- src/examples/json-viewer/JSONViewer.tsx
+++ src/examples/json-viewer/JSONViewer.tsx
@@ -64,8 +64,20 @@ const JSONViewer: React.FC = (): JSX.Element => {
       </div>
       <div className="Viewer-Main">
         <section>
+          <article>
+
           <h4>JSON Tree</h4>
-          <JSONTree depth={0} elemKey={null} elemValue={json} />
+          <details>
+            <summary>Key</summary>
+            <div className="Key-Container">
+                <div className="Json-Attribute-Value-Color">Value</div>
+                <div className="Json-Array-Color">Array</div>
+                <div className="Json-Object-Color">Object</div>
+            </div>
+          </details>
+          
+          <JSONTree isTop={true} elemKey={null} elemValue={json} />
+          </article>
         </section>
       </div>
     </>
